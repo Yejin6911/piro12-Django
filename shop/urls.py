0deedb187cbe19569d7754de7f4a8befa1fc9f28
yejin <cdjin6911@gmail.com>
2020-01-21 01:54:19 +0900

    22 템플릿 상속을 통한 중복 제거

--- shop/urls.py
+++ shop/urls.py
@@ -9,5 +9,8 @@ app_name = 'shop'
 
 urlpatterns = [
     path('archives/<yyyy:year>/', views.archives_year),
-    path('', views.item_list)
+    path('', views.item_list),
+    path('<int:pk>/', views.item_detail),
+    #장고 1버전
+    #re_path(r'^(?P<pk>\d+)/$',views.item_detail),
 ]
