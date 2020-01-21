2126f35d272ea3e143dc046784f12c7c0bc01fba
yejin <cdjin6911@gmail.com>
2020-01-21 16:23:46 +0900

    30 장고 Form을 통해, 글 생성/수정 구현하기

--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,8 @@ import logging
 import re
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
-
+from django.views.generic import CreateView, UpdateView
+from.forms import ItemForm
 from shop.models import Item
 
 
@@ -29,69 +30,83 @@ def item_detail(request,pk):
     })
 
 def item_new(request, item=None):
-    error_list = []
-    initial = {}
-
+    # error_list = []
+    # initial = {}
     if request.method == 'POST':
-        data = request.POST
-        files = request.FILES
-
-        name = data.get('name')
-        desc = data.get('desc')
-        price = data.get('price')
-        photo = files.get('photo')
-        is_published = data.get('is_published') in (True, 't', 'True', '1')
-
-        # 유효성 검사
-        if len(name) < 2:
-            error_list.append('name을 2글자 이상 입력해주세요.')
-
-        if re.match(r'^[\da-zA-Z\s]+$', desc):
-            error_list.append('한글을 입력해주세요.')
-
-        if not error_list:
-            # 저장 시도
-            if item is None:
-                item = Item()
-
-            item.name = name
-            item.desc = desc
-            item.price = price
-            item.is_published = is_published
-
-            if photo:
-                item.photo.save(photo.name, photo, save=False)
-
-            try:
-                item.save()
-            except Exception as e:
-                error_list.append(e)
-            else:
-                return redirect(item)
-
-        initial = {
-            'name': name,
-            'desc': desc,
-            'price': price,
-            'photo': photo,
-            'is_published': is_published,
-        }
+        form = ItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            item = form.save() #ModelForm에서 제공
+            return redirect(item)
     else:
-        if item is not None:
-            initial = {
-                'name': item.name,
-                'desc': item.desc,
-                'price': item.price,
-                'photo': item.photo,
-                'is_published': item.is_published,
-            }
+        form = ItemForm(instance=item)
 
     return render(request, 'shop/item_form.html', {
-        'error_list': error_list,
-        'initial': initial,
+        'form': form,
     })
+    #     data = request.POST
+    #     files = request.FILES
+    #
+    #     name = data.get('name')
+    #     desc = data.get('desc')
+    #     price = data.get('price')
+    #     photo = files.get('photo')
+    #     is_published = data.get('is_published') in (True, 't', 'True', '1')
+    #
+    #     # 유효성 검사
+    #     if len(name) < 2:
+    #         error_list.append('name을 2글자 이상 입력해주세요.')
+    #
+    #     if re.match(r'^[\da-zA-Z\s]+$', desc):
+    #         error_list.append('한글을 입력해주세요.')
+    #
+    #     if not error_list:
+    #         # 저장 시도
+    #         if item is None:
+    #             item = Item()
+    #
+    #         item.name = name
+    #         item.desc = desc
+    #         item.price = price
+    #         item.is_published = is_published
+    #
+    #         if photo:
+    #             item.photo.save(photo.name, photo, save=False)
+    #
+    #         try:
+    #             item.save()
+    #         except Exception as e:
+    #             error_list.append(e)
+    #         else:
+    #             return redirect(item)
+    #
+    #     initial = {
+    #         'name': name,
+    #         'desc': desc,
+    #         'price': price,
+    #         'photo': photo,
+    #         'is_published': is_published,
+    #     }
+    # else:
+    #     if item is not None:
+    #         initial = {
+    #             'name': item.name,
+    #             'desc': item.desc,
+    #             'price': item.price,
+    #             'photo': item.photo,
+    #             'is_published': item.is_published,
+    #         }
+    #
+    # return render(request, 'shop/item_form.html', {
+    #     'error_list': error_list,
+    #     'initial': initial,
+    # })
 
 
 def item_edit(request, pk):
     item = get_object_or_404(Item, pk=pk)
     return item_new(request, item)
+
+#클래스기반 활용
+# item_new = CreateView.as_view(model = Item, form_class=ItemForm)
+
+# # item_edit = UpdateView.as_view(model = Item, form_class=ItemForm)
